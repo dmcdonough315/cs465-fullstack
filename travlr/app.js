cf4cde3f3729b09587244699d876933430ccd1e2
dmcdonough315 <dakota.mcdonough@snhu.edu>
2023-07-23 15:18:13 -0400

    updating

--- travlr/app.js
+++ travlr/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const favicon = require('serve-favicon');
+require('./app_server/models/db');
 
 //updated file paths for new folder placement
 const indexRouter = require('./app_server/routes/index');
